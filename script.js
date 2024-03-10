5a739ecb8b614b94951f71b1c8aeec467a05a7c5
dhonaobina <dhona.obina@outlook.com>
2024-03-10 19:56:47 +1300

    debug as error found from line 11 change | to +

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 var studentArray = new Array ("David", "John", "Michelle", "Sandra", "Alex");
 function dispList() 
 {
-    for (var i=0; i<studentArray.length; i++) 
+    for (var i = 0; i<studentArray.length; i++) 
     {
         var listItem = document.createElement("li");
         var itemText = document.createTextNode (studentArray[i]);
         listItem.appendChild(itemText);
         document.getElementById('studentList').appendChild(listItem);
     }
-    document.getElementById("txtout").innerHTML = "There are " + studentArray.length | " elements in the array."
+    document.getElementById("txtout").innerHTML = "There are " + studentArray.length + " elements in the array."
 }
 
 function clrList() 
